466bdd62879c30274e7a5daf4ccb3b68b62ae193
Tejassree <tejassreerr@gmail.com>
2026-05-01 14:08:50 +0530

    Update root script.js with static data

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-fetch("http://127.0.0.1:8000/api/students/")
-.then(response => response.json())
-.then(data => {
+const data = [
+    {name: "Alice", department: "CS", age: 20},
+    {name: "Bob", department: "IT", age: 22},
+    {name: "Charlie", department: "ME", age: 21}
+];
 
-    let output = "";
-
-    data.forEach(student => {
-        output += `
-            <div class="card">
-                <h2>${student.name}</h2>
-                <p>Department: ${student.department}</p>
-                <p>Age: ${student.age}</p>
-            </div>
-        `;
-    });
-
-    document.getElementById("students").innerHTML = output;
-})
-.catch(error => {
-    console.log(error);
+let output = "";
+data.forEach(student => {
+    output += `
+        <div class="card">
+            <h2>${student.name}</h2>
+            <p>Department: ${student.department}</p>
+            <p>Age: ${student.age}</p>
+        </div>
+    `;
 });
+document.getElementById("students").innerHTML = output;
